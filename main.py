aa1725e84339a5e73a53166548ca022e66821a98
drsmile1001 <drsmile.1001@gmail.com>
2023-04-14 08:31:17 +0800

    init

--- main.py
+++ main.py
@@ -1,11 +1,68 @@
-from fastapi import FastAPI
+from typing import Union
+from fastapi import FastAPI, Form, Response, Request
 
 app = FastAPI()
 
+default_user = "root"
+default_password = "y1ZDezJhorSf5JBbJzpMz08D"
 
-@app.get("/items/")
-def read_item():
-    return ["apple", "banana", "cherry"]
+allow_username = "alice"
+allow_password = "bob"
+
+
+@app.post("/user")
+async def check_user(username: str = Form(), password: str = Form()):
+    if username == default_user and password == default_password:
+        return Response("allow administrator management", status_code=200)
+    if username == allow_username and password == allow_password:
+        return Response(
+            "allow", status_code=200, headers={"Content-Type": "text/plain"}
+        )
+    return Response("deny", status_code=200, headers={"Content-Type": "text/plain"})
+
+
+@app.post(
+    "/vhost",
+)
+async def check_vhost(request: Request):
+    # parse request as plan text
+    body = await request.body()
+    body = body.decode("utf-8")
+
+    print(body)
+    Response("allow", status_code=200)
+    # if username == allow_username or username == default_user:
+    #     return Response(
+    #         "allow", status_code=200, headers={"Content-Type": "text/plain"}
+    #     )
+    # return Response("deny", status_code=200, headers={"Content-Type": "text/plain"})
+
+
+@app.post("/resource")
+async def check_vhost(
+    username: str = Form(),
+    vhost: str = Form(),
+    resource: str = Form(),
+    name: str = Form(),
+    permission: str = Form(),
+):
+    if username == allow_username or username == default_user:
+        return Response("allow", status_code=200)
+    return Response("deny", status_code=200, headers={"Content-Type": "text/plain"})
+
+
+@app.post("/topic")
+async def check_topic(
+    username: str = Form(),
+    vhost: str = Form(),
+    resource: str = Form(),
+    name: str = Form(),
+    permission: str = Form(),
+    routing_key: str = Form(),
+):
+    if username == allow_username or username == default_user:
+        return Response("allow", status_code=200)
+    return Response("deny", status_code=200, headers={"Content-Type": "text/plain"})
 
 
 if __name__ == "__main__":
